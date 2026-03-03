b60f79ceaa1a30fec5cc5dbb350f85c67ae8b942
PREM KUMAR MOKARA <premmokra143@gmail.com>
2026-03-03 13:53:16 +0530

    Fix: remove unsupported encoding param from logger, install fastembed

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ logger.add(
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     level="INFO",
     colorize=True,
-    encoding="utf-8",
 )
 
 # Only add file logger if logs directory is writable
